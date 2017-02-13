2e619bbf4734ba0aef01baef5196ee4c7aeb16e1
ltrias <lucas.trias@gmail.com>
2017-02-13 00:00:01 -0200

    Improving CheckoutController tests

--- test/controllers/CheckoutController.test.js
+++ test/controllers/CheckoutController.test.js
@@ -5,23 +5,23 @@ let controller = new CheckoutController();
 test('Try to checkout without customer', () => {
     expect(() => {
         controller.validateData({});
-    }).toThrowError(/Must have/);
+    }).toThrow(/Must have a customer/);
 });
 
 test('Try to checkout with empty customer', () => {
     expect(() => {
         controller.validateData({"customer":""});
-    }).toThrowError(/Must have/);
+    }).toThrow(/Must have a customer/);
 });
 
 test('Try to checkout without ads', () => {
     expect(() => {
         controller.validateData({"customer":"customer"});
-    }).toThrowError(/Must have/);
+    }).toThrow(/Must have ads/);
 });
 
 test('Try to checkout with empty ads', () => {
     expect(() => {
         controller.validateData({"customer":"customer", "ads":[]});
-    }).toThrowError(/Must have/);
+    }).toThrow(/Must have ads/);
 });
